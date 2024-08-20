67bbfaee9e9c839128ca4a203ba9e08593f614c9
hdai.mir <hadimirzazade.diz@gmail.com>
2024-08-20 18:59:52 +0330

    v1.1.1

--- js/script.js
+++ js/script.js
@@ -3,7 +3,8 @@ let bodyElem = $.querySelector("body").addEventListener("keydown",keyboardAction
 let input = $.getElementById("result");
 let numbersAndActions = ["0","1","2","3","4","5","6","7","8","9",".","=","+","*","**","/","-","%","Backspace","Enter","C","CE",];
 let li1 = $.querySelector(".ce"),li2 = $.querySelector(".power"),li3 = $.querySelector(".rem"),li4 = $.querySelector(".nine"),li5 = $.querySelector(".eight"),li6 = $.querySelector(".seven"),li7 = $.querySelector(".six"),li8 = $.querySelector(".five"),li9 = $.querySelector(".four"),li10 = $.querySelector(".three"),
-li11 = $.querySelector(".two"),li12 = $.querySelector(".one"),li13 = $.querySelector(".equal"),li14 = $.querySelector(".zero"),li15 = $.querySelector(".dot"),li16 = $.querySelector(".comp"),li17 = $.querySelector(".minus"),li18 = $.querySelector(".division"),li19 = $.querySelector(".multi"),li20 = $.querySelector(".plus"),lis = [li1,li2,li3,li4,li5,li6,li8,li9,li10,li11,li12,li13,li14,li15,li16,li17,li18,li19,li20,];
+li11 = $.querySelector(".two"),li12 = $.querySelector(".one"),li13 = $.querySelector(".equal"),li14 = $.querySelector(".zero"),li15 = $.querySelector(".dot"),li16 = $.querySelector(".comp"),li17 = $.querySelector(".minus"),li18 = $.querySelector(".division"),li19 = $.querySelector(".multi"),li20 = $.querySelector(".plus"),
+lis = [li1,li2,li3,li4,li5,li6,li7,li8,li9,li10,li11,li12,li13,li14,li15,li16,li17,li18,li19,li20,];
 for (let i = 0; i < lis.length; i++) {
   lis[i].addEventListener("click", btnDetect);
 }
@@ -83,6 +84,7 @@ function mergeNumbers(op) {
 
 function btnDetect(event) {
   let liElem = event.target;
+  console.log(liElem)
   incomeBtn = liElem.innerHTML;
   if (incomeBtn == "÷") incomeBtn = "/";
   if (incomeBtn == "×") incomeBtn = "*";
