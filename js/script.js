b08fe358fe198e40a9cbdc780693cf46cb8721a9
hdai.mir <hadimirzazade.diz@gmail.com>
2024-08-21 09:05:05 +0330

    v1.2.1

--- js/script.js
+++ js/script.js
@@ -5,16 +5,15 @@ let numbersAndActions = ["0","1","2","3","4","5","6","7","8","9",".","=","+","*"
 let li1 = $.querySelector(".ce"),li2 = $.querySelector(".power"),li3 = $.querySelector(".rem"),li4 = $.querySelector(".nine"),li5 = $.querySelector(".eight"),li6 = $.querySelector(".seven"),li7 = $.querySelector(".six"),li8 = $.querySelector(".five"),li9 = $.querySelector(".four"),li10 = $.querySelector(".three"),
 li11 = $.querySelector(".two"),li12 = $.querySelector(".one"),li13 = $.querySelector(".equal"),li14 = $.querySelector(".zero"),li15 = $.querySelector(".dot"),li16 = $.querySelector(".comp"),li17 = $.querySelector(".minus"),li18 = $.querySelector(".division"),li19 = $.querySelector(".multi"),li20 = $.querySelector(".plus"),
 lis = [li1,li2,li3,li4,li5,li6,li7,li8,li9,li10,li11,li12,li13,li14,li15,li16,li17,li18,li19,li20,];
-for (let i = 0; i < lis.length; i++) {
+for (let i = 0; i < lis.length; i++)
+{
   lis[i].addEventListener("click", btnDetect);
 }
 let firstNumber = "",secondNumber = "",action,actionCounter = 0,incomeBtn = "";
-
 function keyboardAction(event){
   let keyValue = event.key;
   actionDetect(keyValue);
 }
-
 function mergeNumbers(op) {
   switch (op) {
     case "+":
@@ -50,7 +49,6 @@ function mergeNumbers(op) {
   console.log("merge");
   return firstNumber;
 }
-
 function btnDetect(event) {
   let liElem = event.target;
   incomeBtn = liElem.innerHTML;
@@ -60,54 +58,58 @@ function btnDetect(event) {
   if (incomeBtn == "−") incomeBtn = "-";
   actionDetect(incomeBtn);
 }
-
-
-function actionDetect(incomeAction) {
+function actionDetect(incomeAction)
+{
   let isAction = false,isNumber = false;
-  for (let i = 0; i < numbersAndActions.length; i++) {
-    if (incomeAction == numbersAndActions[i]) {
+  for (let i = 0; i < numbersAndActions.length; i++)
+  {
+    if (incomeAction == numbersAndActions[i])
+    {
       if (i < 10) isNumber = true;
       else isAction = true;
     }
   }
-  if (isNumber) {
-    if (actionCounter == 0) {
+  if(isNumber){
+    if(actionCounter == 0)
+    {
       firstNumber += incomeAction;
       input.value = firstNumber;
     }
-    if (actionCounter == 1) {
+    if(actionCounter == 1)
+    {
       input.value = "";
       secondNumber += incomeAction;
       input.value = secondNumber;
     }
   }
-  if (isAction && firstNumber.length > 0) {
-    if (secondNumber.length == 0) {
-      if (incomeAction == "=" ||incomeAction == "Enter" ||incomeAction == "Backspace")
-        {
+  if(isAction && firstNumber.length > 0) 
+  {
+    if(secondNumber.length == 0) {
+      if(incomeAction == "=" ||incomeAction == "Enter" ||incomeAction == "Backspace")
+      {
         actionCounter = 1;
       }
        else {
               actionCounter = 1;
               action = incomeAction;
-        if (incomeAction == "C" || incomeAction == "CE")
+        if(incomeAction == "C" || incomeAction == "CE")
         {
           mergeNumbers(action);
           actionCounter = 0;
           action = "";
         }
       }
-    } else {
-      if (incomeAction == "C" || incomeAction == "CE")
-        {
+    } 
+    else  { if(incomeAction == "C" || incomeAction == "CE")
+      {
         mergeNumbers(incomeAction);
         actionCounter = 1;
       }
       else 
-      input.value = mergeNumbers(action);
-      secondNumber = "";
-      action = incomeAction;
-      actionCounter = 1;
+        input.value = mergeNumbers(action);
+        secondNumber = "";
+        action = incomeAction;
+        actionCounter = 1;
     }
   }
 }
